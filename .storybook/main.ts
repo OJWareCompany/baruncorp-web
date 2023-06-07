1085d2b780b8ab866d8a8d61aedd9e708835b790
Yunwoo Ji <yunwoo@ojware.com>
2023-06-07 18:46:02 +0900

    🐳 chore: set up storybook to enable component development

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,20 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import { TsconfigPathsPlugin } from "tsconfig-paths-webpack-plugin";
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-styling",
+      options: {
+        // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
+        // For more details on this addon's options.
+        postCss: true,
+      },
+    },
   ],
   framework: {
     name: "@storybook/nextjs",
@@ -13,5 +23,13 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  webpackFinal: async (config) => ({
+    ...config,
+    resolve: {
+      ...config.resolve,
+      plugins: [...(config.resolve?.plugins ?? []), new TsconfigPathsPlugin()],
+    },
+  }),
 };
+
 export default config;
