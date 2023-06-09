feea28c02aaa214d465f4e865642db62d1a48253
Yunwoo Ji <yunwoo@ojware.com>
2023-06-10 03:08:01 +0900

    Revert "✨ feat: develop common label component"
    This reverts commit 5f5548f10f56a41a616b743202a9825d5eef9c7c.

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -7,24 +7,17 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
-  {
-    variants: {
-      required: {
-        true: "after:content-['*'] after:text-destructive after:ml-1",
-      },
-    },
-  }
+  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
 );
 
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
     VariantProps<typeof labelVariants>
->(({ className, required = false, ...props }, ref) => (
+>(({ className, ...props }, ref) => (
   <LabelPrimitive.Root
     ref={ref}
-    className={cn(labelVariants({ required }), className)}
+    className={cn(labelVariants(), className)}
     {...props}
   />
 ));
