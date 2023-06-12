6d8fdd72a4717141b2be5635e8963deee816236e
Yunwoo Ji <yunwoo@ojware.com>
2023-06-12 19:57:47 +0900

    🌈 style: change the style of the focused input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -4,11 +4,11 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const inputVariants = cva(
-  "flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline focus-visible:outline-input disabled:cursor-not-allowed disabled:opacity-50 read-only:focus-visible:outline-none",
+  "flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
   {
     variants: {
       error: {
-        true: "border-destructive focus-visible:outline-destructive",
+        true: "border-destructive focus-visible:ring-destructive",
       },
     },
   }
