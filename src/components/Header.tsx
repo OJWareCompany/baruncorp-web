9dc1cf75cff783185c32e6afee34c56b2ae009e4
Yunwoo Ji <yunwoo@ojware.com>
2023-06-12 21:04:18 +0900

    🐞 fix: modify header UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import { LogOut, UserCog2 } from "lucide-react";
+import { LogOut, User2 } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
@@ -25,24 +28,30 @@ export default function Header() {
       <div className="container flex h-16 items-center justify-between">
         <h1 className="h2">Barun Corp</h1>
         <DropdownMenu>
-          <DropdownMenuTrigger asChild={true}>
-            <div className="flex gap-2 items-center cursor-pointer">
-              <Avatar>
+          <DropdownMenuTrigger asChild>
+            <Button variant={"ghost"} className="relative h-8 w-8 rounded-full">
+              <Avatar className="h-9 w-9">
                 <AvatarFallback>El</AvatarFallback>
               </Avatar>
-              <div className="flex flex-col">
-                <span className="p">Elon Musk</span>
-                <span className="muted">elonmusk@tesla.com</span>
-              </div>
-            </div>
+            </Button>
           </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56">
+          <DropdownMenuContent className="w-56" align="end">
+            <DropdownMenuLabel className="font-normal">
+              <div className="flex flex-col gap-1">
+                <span className="sm-500 leading-none">Elon Musk</span>
+                <span className="text-xs leading-none text-muted-foreground">
+                  elonmusk@tesla.com
+                </span>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
             <DropdownMenuItem asChild>
               <Link href="/my-preferences">
-                <UserCog2 className="mr-2 h-4 w-4" />
-                <span>My Preferences</span>
+                <User2 className="mr-2 h-4 w-4" />
+                <span>Profile</span>
               </Link>
             </DropdownMenuItem>
+            <DropdownMenuSeparator />
             <DropdownMenuItem onClick={handleSignOutButtonClick}>
               <LogOut className="mr-2 h-4 w-4" />
               <span>Sign Out</span>
