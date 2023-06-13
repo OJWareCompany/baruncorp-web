1e12613068bc26849279dd4e8e2ebe1acffa7a02
leesangwon <sangwon@ojware.com>
2023-06-14 00:51:06 +0900

    ✨ feat: add default access control with next-auth

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,8 +1,19 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 import Header from "@/components/Header";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await getServerSession(authOptions);
+  if (!session) redirect("/signin");
+
   return (
     <>
+      {/* <pre>{JSON.stringify(session, null, 2)}</pre> */}
       <Header />
       <main className="container py-8">{children}</main>
     </>
