ff2b19ead24a5cc833f84dcc417545ab57f91c70
leesangwon <sangwon@ojware.com>
2023-06-14 00:49:27 +0900

    ✨ feat: add signup page

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,204 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { AlertCircle } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { Loader2 } from "lucide-react";
+
+import Link from "next/link";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  firstName: z.string().trim().min(1, { message: "First Name is required" }),
+  lastName: z.string().trim().min(1, { message: "Last Name is required" }),
+  email: z
+    .string()
+    .trim()
+    .min(1, { message: "Email is required" })
+    .email({ message: "Email Address is not correct" }),
+  password: z
+    .string()
+    .trim()
+    .min(1, { message: "Password is required" })
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!])(?!.*\s).{12,}$/, {
+      message:
+        "Minimum 12 characters with at least 1 number, uppercase, and lowercase letter, special characters",
+    }),
+  code: z.string().trim().min(1, { message: "Code is required" }),
+});
+
+export default function SignupPage() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      code: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setError(false);
+    setLoading(true);
+    const { email, password } = values;
+    if (email.split("@")[0] === password) {
+      setLoading(false);
+      form.control.setError("password", {
+        message: "Email ID cannot be a password.",
+      });
+      return;
+    }
+
+    /**
+     * TODO 회원가입 API 연동
+     * - 성공 -> 로그인 페이지로 이동
+     * - 실패 -> 서버에서 내려주는 에러 메시지 보여주기
+     */
+    const signup = new Promise((resolve) => {
+      console.log(form);
+      setTimeout(() => {
+        resolve({
+          result: values.email === "oj@kakao.com" ? "success" : "fail",
+        });
+      }, 2500);
+    });
+    signup.then((resolve: any) => {
+      if (resolve.result === "success") {
+        router.push("/signin");
+      } else {
+        setLoading(false);
+        setError(true);
+      }
+    });
+  }
+
+  return (
+    <>
+      <Alert
+        variant="destructive"
+        className={`fixed w-96 top-8 left-2/4 translate-x-[-50%] ${
+          !error && "invisible"
+        }`}
+      >
+        <AlertCircle className="h-4 w-4" />
+        <AlertTitle>Invalid code</AlertTitle>
+        <AlertDescription>
+          Please check your mail again or contact the Barun corp staff.
+        </AlertDescription>
+      </Alert>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="firstName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel required={true}>First Name</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="lastName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel required={true}>Last Name</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel required={true}>Email Address</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel required={true}>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="code"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel required={true}>Code</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" fullWidth={true} disabled={loading}>
+            {loading ? (
+              <>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Please wait
+              </>
+            ) : (
+              "Submit"
+            )}
+          </Button>
+          <div className="border border-input" />
+          <Button
+            type="button"
+            variant="outline"
+            fullWidth={true}
+            disabled={loading}
+          >
+            {loading ? (
+              <>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Please wait
+              </>
+            ) : (
+              <Link href="/signin">Sign in</Link>
+            )}
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+}
