a7383c70721beabf7f8ef06871f85251b1a645ef
Yunwoo Ji <yunwoo@ojware.com>
2023-06-14 12:45:13 +0900

    🌈 style: modify margin of title

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -12,7 +12,7 @@ export default async function AuthLayout({
   return (
     <>
       <main className="w-96 mx-auto flex flex-col justify-center">
-        <h1 className="h1 mb-12 mt-48 text-center">Barun Corp</h1>
+        <h1 className="h1 mb-12 mt-40 text-center">Barun Corp</h1>
         {children}
       </main>
     </>
