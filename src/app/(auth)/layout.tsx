cc4df94d66d5b4ed0a8fc61249d1e843f5319ac2
leesangwon <sangwon@ojware.com>
2023-06-20 00:15:19 +0900

    ✨ feat: implement RoutingGuard

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,20 +1,12 @@
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-import { authOptions } from "../api/auth/[...nextauth]/route";
-
 export default async function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession(authOptions);
-  if (session) return redirect("/");
   return (
-    <>
-      <main className="w-96 mx-auto flex flex-col justify-center">
-        <h1 className="h1 mb-12 mt-40 text-center">Barun Corp</h1>
-        {children}
-      </main>
-    </>
+    <main className="w-96 mx-auto flex flex-col justify-center">
+      <h1 className="h1 mb-12 mt-40 text-center">Barun Corp</h1>
+      {children}
+    </main>
   );
 }
