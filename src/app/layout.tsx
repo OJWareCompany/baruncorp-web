4f9fe5c82dec882d38ab870a3c88df0a5c22e611
leesangwon <sangwon@ojware.com>
2023-06-19 16:39:27 +0900

    🐳 chore: setup react-query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthContext from "@/context/AuthContext";
+import QueryContext from "@/context/QueryContext";
 import { Toaster } from "@/components/Toaster";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} w-full`}>
-        <AuthContext>{children}</AuthContext>
+        <AuthContext>
+          <QueryContext>{children}</QueryContext>
+        </AuthContext>
         <Toaster />
       </body>
     </html>
---
+++ src/context/QueryContext.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const queryClient = new QueryClient();
+
+export default function AuthContext({ children }: Props) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  );
+}
