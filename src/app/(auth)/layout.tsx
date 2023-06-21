b02d70eb6f102575c771bb878834d06fe0c83b92
Yunwoo Ji <yunwoo@ojware.com>
2023-06-21 12:43:57 +0900

    🌈 style: fix a little style

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -7,8 +7,8 @@ export default async function AuthLayout({
 }) {
   return (
     <RoutingGuard authenticated={false}>
-      <main className="w-96 mx-auto flex flex-col justify-center">
-        <h1 className="h1 mb-12 mt-40 text-center">Barun Corp</h1>
+      <main className="w-96 mx-auto flex flex-col justify-center py-32">
+        <h1 className="h1 mb-12 text-center">Barun Corp.</h1>
         {children}
       </main>
     </RoutingGuard>
