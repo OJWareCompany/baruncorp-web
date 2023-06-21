7b7c1f90332239b729214ae47a78bf099ca331bb
Yunwoo Ji <yunwoo@ojware.com>
2023-06-21 16:54:59 +0900

    🐞 fix: improve routing guard

--- src/components/RoutingGuard.tsx
+++ src/components/RoutingGuard.tsx
@@ -3,6 +3,7 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { Loader2 } from "lucide-react";
+import { useEffect } from "react";
 
 interface Props {
   children: React.ReactNode;
@@ -13,22 +14,28 @@ export default function RoutingGuard({ children, authenticated }: Props) {
   const router = useRouter();
   const { status } = useSession();
 
-  if (status === "loading") {
+  useEffect(() => {
+    if (status === "authenticated" && !authenticated) {
+      router.replace("/");
+      return;
+    }
+
+    if (status === "unauthenticated" && authenticated) {
+      router.replace("/signin");
+      return;
+    }
+  }, [authenticated, router, status]);
+
+  if (
+    status === "loading" ||
+    (status === "authenticated" && !authenticated) ||
+    (status === "unauthenticated" && authenticated)
+  ) {
     return (
       <div className="absolute inset-0 flex items-center justify-center">
         <Loader2 className="h-16 w-16 animate-spin" />
       </div>
     );
-  }
-
-  if (status === "authenticated" && !authenticated) {
-    router.replace("/");
-    return;
-  }
-
-  if (status === "unauthenticated" && authenticated) {
-    router.replace("/signin");
-    return;
   }
 
   return children;
