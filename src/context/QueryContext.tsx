a381464d5ceacfd41480da5868da91c9ed729c15
leesangwon <sangwon@ojware.com>
2023-06-23 14:26:57 +0900

    ✨ feat: add react-query retry policy to handle unauthrized case

--- src/context/QueryContext.tsx
+++ src/context/QueryContext.tsx
@@ -2,14 +2,30 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { isAxiosError } from "axios";
 
 type Props = {
   children: React.ReactNode;
 };
 
-const queryClient = new QueryClient();
+function retry(failureCount: number, error: unknown) {
+  if (failureCount > 2) {
+    return false;
+  }
+  if (isAxiosError(error) && error.response?.data?.errorCode === "10005") {
+    return false;
+  }
+  return true;
+}
 
-export default function AuthContext({ children }: Props) {
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { retry },
+    mutations: { retry },
+  },
+});
+
+export default function QueryContext({ children }: Props) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
