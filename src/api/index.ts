5ef5919712e0fc007aa1e5294202537c95098e24
Yunwoo Ji <yunwoo@ojware.com>
2023-06-26 17:46:02 +0900

    🦄 refactor: improve api client

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,10 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import axios from "axios";
 
-const instance = axios.create({
+const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     Accept: "application/json",
   },
 });
-
-const apiClient = <T = any, D = any>(config: AxiosRequestConfig<D>) =>
-  instance.request<T, AxiosResponse<T, D>, D>(config);
 
 export default apiClient;
