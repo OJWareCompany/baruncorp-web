7c202f85289a0659e66627a7d4ab68244339c7fa
Yunwoo Ji <yunwoo@ojware.com>
2023-06-28 12:25:07 +0900

    🦄 refactor: improve useQuery, useMutation hook by making custom hook

---
+++ src/queries/useProfileMutation.ts
@@ -0,0 +1,31 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useSession } from "next-auth/react";
+import { AxiosError, AxiosResponse } from "axios";
+import { QUERY_KEY as profileQueryKey } from "./useProfileQuery";
+import apiClient from "@/api";
+import { ProfilePatchReqDto } from "@/types/dto/users";
+
+const useProfileMutation = () => {
+  const { data: session } = useSession();
+  const queryClient = useQueryClient();
+
+  return useMutation<
+    AxiosResponse<any>,
+    AxiosError<ErrorResponseData>,
+    ProfilePatchReqDto
+  >(
+    (data) =>
+      apiClient.patch("/users/profile", data, {
+        headers: {
+          Authorization: `Bearer ${session?.accessToken}`,
+          "Content-Type": "application/json",
+        },
+      }),
+    {
+      onSuccess: () =>
+        queryClient.invalidateQueries({ queryKey: [profileQueryKey] }),
+    }
+  );
+};
+
+export default useProfileMutation;
---
+++ src/types/dto/users.ts
@@ -0,0 +1,12 @@
+export interface ProfileGetResDto {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  companyId: number;
+}
+
+export interface ProfilePatchReqDto {
+  firstName: string;
+  lastName: string;
+}
