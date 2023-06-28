e3f266b3af6f766763b209515ed6b16b68596220
Yunwoo Ji <yunwoo@ojware.com>
2023-06-28 12:25:07 +0900

    🦄 refactor: improve useQuery, useMutation hook by making custom hook

--- src/app/(root)/profile/page.tsx
+++ src/app/(root)/profile/page.tsx
@@ -3,11 +3,10 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { AxiosError, AxiosResponse } from "axios";
 import { useSession } from "next-auth/react";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import axios from "axios";
+import { useQueryClient } from "@tanstack/react-query";
 import {
   Form,
   FormControl,
@@ -19,6 +18,8 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { toast } from "@/hook/use-toast";
+import useProfileQuery from "@/queries/useProfileQuery";
+import useProfileMutation from "@/queries/useProfileMutation";
 
 const formSchema = z.object({
   firstName: z.string().trim().min(1, { message: "First Name is required" }),
@@ -27,19 +28,9 @@ const formSchema = z.object({
   organization: z.string(),
 });
 
-interface Profile {
-  id: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  companyId: number;
-}
-
-interface ProfilePatchData extends Pick<Profile, "firstName" | "lastName"> {}
-
 export default function ProfilePage() {
-  const { data: session, update } = useSession();
-  const queryClient = useQueryClient();
+  const { update } = useSession();
+  const [isAccessTokenError, setIsAccessTokenError] = useState(false);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -53,71 +44,33 @@ export default function ProfilePage() {
     control,
     reset,
     handleSubmit,
-    formState: { isSubmitting, isDirty: isEdit },
+    formState: { isSubmitting, isDirty },
   } = form;
 
-  /**
-   * get profile query
-   */
-  const profileQuery = useQuery<Profile, AxiosError<ErrorResponseData>>({
-    queryKey: ["profile", session?.accessToken, session?.isValid],
-    queryFn: async () => {
-      const response = await axios.get(
-        `${process.env.NEXT_PUBLIC_API_URL}/users/profile`,
-        {
-          headers: {
-            Authorization: `Bearer ${session?.accessToken}`,
-          },
-        }
-      );
-      return response.data;
-    },
-    refetchOnWindowFocus: false,
-    enabled: session?.accessToken != null && session.isValid,
-  });
-
-  /**
-   * update profile mutation
-   */
-  const mProfile = useMutation<
-    AxiosResponse<Profile>,
-    AxiosError<ErrorResponseData>,
-    ProfilePatchData
-  >({
-    mutationFn: (data) => {
-      return axios.patch(
-        `${process.env.NEXT_PUBLIC_API_URL}/users/profile`,
-        data,
-        {
-          headers: {
-            Authorization: `Bearer ${session?.accessToken}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-    },
-  });
-
-  const { data: profile } = profileQuery;
+  const { data: profile, isSuccess, isError, error } = useProfileQuery();
+  const { mutateAsync } = useProfileMutation();
 
   useEffect(() => {
-    if (profileQuery.isSuccess && profile && !isEdit) {
-      const { email, firstName, lastName, companyId } = profile;
-      reset({
-        firstName,
-        lastName,
-        email,
-        organization: companyId === 1 ? "BARUN CORP" : "TESLA", // TODO 서버측에서 넘겨주는 데이터 변경된 이후 다시 확인
-      });
-    }
-  }, [isEdit, profile, profileQuery.isSuccess, reset]);
-
-  useEffect(() => {
-    if (!profileQuery.isError || profileQuery.error.response?.data == null) {
+    if (!isSuccess || isAccessTokenError) {
       return;
     }
 
-    if (profileQuery.error.response.data.errorCode === "10005") {
+    const { email, firstName, lastName, companyId } = profile;
+    reset({
+      firstName,
+      lastName,
+      email,
+      organization: companyId === 1 ? "BARUN CORP" : "TESLA", // TODO 서버측에서 넘겨주는 데이터 변경된 이후 다시 확인
+    });
+  }, [isAccessTokenError, isSuccess, profile, reset]);
+
+  useEffect(() => {
+    if (!isError || error.response == null) {
+      return;
+    }
+
+    const { errorCode, statusCode } = error.response.data;
+    if (errorCode === "10005") {
       update();
       return;
     }
@@ -126,26 +79,29 @@ export default function ProfilePage() {
     let description =
       "Please try again in a few minutes. If the problem persists, please contact the Barun Corp Manager.";
 
-    if (profileQuery.error.response.data.statusCode === 500) {
+    if (statusCode === 500) {
       title = "Server error";
     }
 
     toast({ title, description, variant: "destructive" });
-  }, [profileQuery.isError, profileQuery.error, update]);
+  }, [error, isError, update]);
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
     const { firstName, lastName } = values;
 
-    await mProfile
-      .mutateAsync({ firstName, lastName })
-      .then(() => {
-        queryClient.invalidateQueries({ queryKey: ["profile"] });
-      })
-      .catch(async (error: AxiosError<ErrorResponseData>) => {
-        const { response: errorResponse } = error;
-        if (errorResponse?.data.errorCode === "10005") {
+    setIsAccessTokenError(false);
+    await mutateAsync({ firstName, lastName }).catch(
+      async (error: AxiosError<ErrorResponseData>) => {
+        const { response } = error;
+        if (response == null) {
+          return;
+        }
+
+        const { errorCode, statusCode } = response.data;
+        if (errorCode === "10005") {
           const newSession = await update();
           if (newSession?.isValid) {
+            setIsAccessTokenError(true);
             toast({
               title: "Expired session",
               description: "Please try agian.",
@@ -159,12 +115,13 @@ export default function ProfilePage() {
         let description =
           "Please try again in a few minutes. If the problem persists, please contact the Barun Corp Manager.";
 
-        if (errorResponse?.data.statusCode === 500) {
+        if (statusCode === 500) {
           title = "Server error";
         }
 
         toast({ title, description, variant: "destructive" });
-      });
+      }
+    );
   }
 
   return (
@@ -226,7 +183,7 @@ export default function ProfilePage() {
         <Button
           type="submit"
           fullWidth
-          disabled={!isEdit}
+          disabled={!isDirty}
           loading={isSubmitting}
         >
           Save
---
+++ src/queries/useProfileQuery.ts
@@ -0,0 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { useSession } from "next-auth/react";
+import { ProfileGetResDto } from "@/types/dto/users";
+import apiClient from "@/api";
+
+export const QUERY_KEY = "profile";
+
+const useProfileQuery = () => {
+  const { data: session } = useSession();
+
+  return useQuery<ProfileGetResDto, AxiosError<ErrorResponseData>>({
+    queryKey: [QUERY_KEY, session?.accessToken, session?.isValid],
+    queryFn: () =>
+      apiClient
+        .get("/users/profile", {
+          headers: {
+            Authorization: `Bearer ${session?.accessToken}`,
+          },
+        })
+        .then(({ data }) => data),
+    refetchOnWindowFocus: false, // TODO: 이후에 모든 query에 적용할지 논의 필요
+    enabled: session?.accessToken != null && session.isValid,
+  });
+};
+
+export default useProfileQuery;
