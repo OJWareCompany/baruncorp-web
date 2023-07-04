93802b352f6fa6eda0c4071e08fdce285a115abb
leesangwon <sangwon@ojware.com>
2023-07-05 01:27:35 +0900

    ✨ feat: implement services page

--- src/app/(root)/common/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import React from "react";
-
-export default function Page() {
-  return <div className="h3">Common</div>;
-}
---
+++ src/types/dto/service.ts
@@ -0,0 +1,5 @@
+export type ServicesGetResDto = {
+  id: string;
+  name: string;
+  description: string;
+}[];
