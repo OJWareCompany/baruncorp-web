c465c4e86f2c63d1acf2796ba345e4a5d05e64cd
Yunwoo Ji <yunwoo@ojware.com>
2023-07-05 17:52:13 +0900

    🐞 fix: replace header's nav UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@
 import { LogOut, User2 } from "lucide-react";
 import Link from "next/link";
 import { signOut } from "next-auth/react";
-import React, { SyntheticEvent, useState } from "react";
+import React from "react";
 import { Button } from "./ui/button";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import {
@@ -14,24 +14,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu";
 import useProfileQuery from "@/queries/useProfileQuery";
-import { cn } from "@/lib/utils";
-
-const navItems = [
-  { name: "Project Intake Portal", pathname: "/project-intake-portal" },
-  { name: "Project Management", pathname: "/project-management" },
-  { name: "Invoice", pathname: "/invoice" },
-];
 
 const peopleOperationsNavItems: { name: string; pathname: string }[] = [
   {
@@ -62,16 +45,8 @@ const commonNavItems: { name: string; pathname: string }[] = [
 export default function Header() {
   const { data: profile } = useProfileQuery();
 
-  const fullName = profile && `${profile.firstName} ${profile.lastName}`; // TODO: `GET /users/profile`의 응답이 fullName을 내려주면 개선한다.
-
   const handleSignOutButtonClick = () => {
     signOut({ redirect: false });
-  };
-
-  const [leftOfHoveredMenu, setLeftOfHoveredMenu] = useState<number>(0);
-
-  const handleMouseEnter = (event: SyntheticEvent) => {
-    setLeftOfHoveredMenu(event.currentTarget.getBoundingClientRect().x);
   };
 
   return (
@@ -81,105 +56,76 @@ export default function Header() {
           <Link href="/">
             <h1 className="h3 pointer-events-none">Barun Corp.</h1>
           </Link>
-          <NavigationMenu>
-            <NavigationMenuList>
-              <NavigationMenuItem onMouseEnter={handleMouseEnter}>
-                <NavigationMenuTrigger>People operations</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  <ul className="py-2 w-48">
-                    {peopleOperationsNavItems.map((item) => (
-                      <NavigationMenuItem key={item.pathname}>
-                        <Link href={item.pathname} legacyBehavior passHref>
-                          <NavigationMenuLink
-                            className={cn(
-                              navigationMenuTriggerStyle(),
-                              "rounded-none w-full justify-start"
-                            )}
-                          >
-                            {item.name}
-                          </NavigationMenuLink>
-                        </Link>
-                      </NavigationMenuItem>
-                    ))}
-                  </ul>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-              {navItems.map((item) => (
-                <NavigationMenuItem key={item.pathname}>
-                  <Link href={item.pathname} legacyBehavior passHref>
-                    <NavigationMenuLink
-                      className={navigationMenuTriggerStyle()}
-                    >
-                      {item.name}
-                    </NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-              ))}
-              <NavigationMenuItem onMouseEnter={handleMouseEnter}>
-                <NavigationMenuTrigger>Common</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  <ul className="py-2 w-48">
-                    {commonNavItems.map((item) => (
-                      <NavigationMenuItem key={item.pathname}>
-                        <Link href={item.pathname} legacyBehavior passHref>
-                          <NavigationMenuLink
-                            className={cn(
-                              navigationMenuTriggerStyle(),
-                              "rounded-none w-full justify-start"
-                            )}
-                          >
-                            {item.name}
-                          </NavigationMenuLink>
-                        </Link>
-                      </NavigationMenuItem>
-                    ))}
-                  </ul>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-
-              <NavigationMenuViewport
-                style={{
-                  transform: `translateX(${leftOfHoveredMenu}px)`,
-                }}
-              />
-            </NavigationMenuList>
-          </NavigationMenu>
+          <nav>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant={"ghost"}>People Opertaions</Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="start">
+                {peopleOperationsNavItems.map((item) => (
+                  <DropdownMenuItem asChild key={item.pathname}>
+                    <Link href={item.pathname}>
+                      <span>{item.name}</span>
+                    </Link>
+                  </DropdownMenuItem>
+                ))}
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant={"ghost"}>Common</Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="start">
+                {commonNavItems.map((item) => (
+                  <DropdownMenuItem asChild key={item.pathname}>
+                    <Link href={item.pathname}>
+                      <span>{item.name}</span>
+                    </Link>
+                  </DropdownMenuItem>
+                ))}
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </nav>
         </div>
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            {fullName && (
+            {profile?.fullName && (
               <Button
                 variant={"ghost"}
-                className="relative h-8 w-8 rounded-full animate-in fade-in"
+                className="relative h-10 w-10 rounded-full animate-in fade-in"
               >
-                <Avatar className="h-9 w-9">
-                  <AvatarFallback>{fullName.slice(0, 2)}</AvatarFallback>
+                <Avatar className="h-10 w-10">
+                  <AvatarFallback>
+                    {profile.fullName.slice(0, 2)}
+                  </AvatarFallback>
                 </Avatar>
               </Button>
             )}
           </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56" align="end">
-            <DropdownMenuLabel className="font-normal">
-              <div className="flex flex-col gap-1">
-                <span className="sm">{fullName}</span>
-                <span className="text-xs leading-none text-muted-foreground">
-                  {profile?.email}
-                </span>
-              </div>
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem asChild>
-              <Link href="/profile">
-                <User2 className="mr-2 h-4 w-4" />
-                <span>Profile</span>
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem onClick={handleSignOutButtonClick}>
-              <LogOut className="mr-2 h-4 w-4" />
-              <span>Sign Out</span>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
+          {profile && (
+            <DropdownMenuContent className="w-56" align="end">
+              <DropdownMenuLabel className="font-normal">
+                <div className="flex flex-col gap-1">
+                  <span className="sm">{profile.fullName}</span>
+                  <span className="text-xs leading-none text-muted-foreground">
+                    {profile.email}
+                  </span>
+                </div>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem asChild>
+                <Link href="/profile">
+                  <User2 className="mr-2 h-4 w-4" />
+                  <span>Profile</span>
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={handleSignOutButtonClick}>
+                <LogOut className="mr-2 h-4 w-4" />
+                <span>Sign Out</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          )}
         </DropdownMenu>
       </div>
     </header>
