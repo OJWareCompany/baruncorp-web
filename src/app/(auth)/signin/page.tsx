c99f673742a954c53d1d61e877988931997b88f7
Yunwoo Ji <yunwoo@ojware.com>
2023-07-05 11:55:37 +0900

    🐳 chore: reinitialize shadcn/ui

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -3,9 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
-
 import { signIn } from "next-auth/react";
-import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import {
