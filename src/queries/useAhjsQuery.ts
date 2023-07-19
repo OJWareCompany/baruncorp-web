bb24528ce587e67be7a4e66c2d6d1b7cc92b1939
leesangwon <sangwon@ojware.com>
2023-07-19 14:42:26 +0900

    ✨ feat: integrate api (GET /georaphy/notes)

--- src/queries/useAhjsQuery.ts
+++ src/queries/useAhjsQuery.ts
@@ -10,29 +10,10 @@ const useAhjsQuery = () => {
 
   return useQuery<AhjsGetResDto, AxiosError<ErrorResponseData>>({
     queryKey: [QUERY_KEY],
-    // queryFn: () =>
-    //     apiClient
-    //         .get<AhjsGetResDto>("/geography/notes")
-    //         .then(({ data }) => data),
-    queryFn: () => {
-      return [
-        {
-          id: "1",
-          name: "hello world",
-          modifiedBy: "dfaf",
-        },
-        {
-          id: "12",
-          name: "hello world",
-          modifiedBy: "dfaf",
-        },
-        {
-          id: "123",
-          name: "hello world",
-          modifiedBy: "dfaf",
-        },
-      ];
-    },
+    queryFn: () =>
+      apiClient
+        .get<AhjsGetResDto>("/geography/notes?pageNo=1") // TODO: Paginzation 적용해서 api 호출
+        .then(({ data }) => data),
     refetchOnWindowFocus: false, // TODO: 이후에 모든 query에 적용할지 논의 필요
   });
 };
