4332b863d49d7017267824af1559058b518051b9
leesangwon <sangwon@ojware.com>
2023-07-20 11:57:26 +0900

    🦄 refactor: modularize common profile query invalidation logic

--- src/queries/useDeleteUserLicenseMutation.ts
+++ src/queries/useDeleteUserLicenseMutation.ts
@@ -1,16 +1,12 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
-import useProfileQuery, {
-  QUERY_KEY as profileQueryKey,
-} from "./useProfileQuery";
 import useApiClient from "@/hook/useApiClient";
 import { UserLicenseDeleteReqDto } from "@/types/dto/departments";
+import useProfileQueryInvalidation from "@/hook/useProfileQueryInvalidation";
 
 const useDeleteUserLicenseMutation = (userId: string | undefined) => {
   const apiClient = useApiClient();
-  const { data: myProfile, isSuccess: isMyProfileQuerySuccess } =
-    useProfileQuery();
-  const queryClient = useQueryClient();
+  const invalidate = useProfileQueryInvalidation();
 
   return useMutation<
     void,
@@ -35,19 +31,7 @@ const useDeleteUserLicenseMutation = (userId: string | undefined) => {
         .then(({ data }) => data);
     },
     {
-      onSuccess: () => {
-        if (!isMyProfileQuerySuccess || userId == null) {
-          return;
-        }
-
-        if (myProfile.id === userId) {
-          queryClient.invalidateQueries({
-            queryKey: [profileQueryKey, "mine"],
-          });
-        }
-
-        queryClient.invalidateQueries({ queryKey: [profileQueryKey, userId] });
-      },
+      onSuccess: () => invalidate(userId),
     }
   );
 };
