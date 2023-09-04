d64be866ed1886fb6ef219fed4b6360c62ea3944
Yunwoo Ji <yunwoo@ojware.com>
2023-09-04 15:36:18 +0900

    🐳 chore: reinitialize input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,65 +1,22 @@
-"use client";
-
 import * as React from "react";
+
 import { cn } from "@/lib/utils";
-import { useProvidedRefOrCreate } from "@/hook/useProvidedRefOrCreate";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
-  trailing?: React.ReactElement;
-}
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-/**
- * Github Primer를 참고함
- * https://github.com/primer/react/blob/main/src/TextInput/TextInput.tsx
- */
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, trailing, onFocus, onBlur, ...props }, ref) => {
-    const [isInputFocused, setIsInputFocused] = React.useState(false);
-    const inputRef = useProvidedRefOrCreate(
-      ref as React.RefObject<HTMLInputElement>
-    );
-
-    const focusInput: React.MouseEventHandler = () => {
-      inputRef.current?.focus();
-    };
-    const handleInputFocus = React.useCallback(
-      (e: React.FocusEvent<HTMLInputElement>) => {
-        setIsInputFocused(true);
-        onFocus && onFocus(e);
-      },
-      [onFocus]
-    );
-    const handleInputBlur = React.useCallback(
-      (e: React.FocusEvent<HTMLInputElement>) => {
-        setIsInputFocused(false);
-        onBlur && onBlur(e);
-      },
-      [onBlur]
-    );
-
+  ({ className, type, ...props }, ref) => {
     return (
-      <div
+      <input
+        type={type}
         className={cn(
-          "flex items-center h-10 w-full rounded-md border border-input bg-transparent text-sm ring-offset-background data-[focused=true]:ring-2 data-[focused=true]:ring-ring data-[focused=true]:ring-offset-2 data-[disabled=true]:cursor-not-allowed data-[disabled=true]:opacity-50",
-          trailing && "pr-3",
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
-        data-disabled={props.disabled}
-        data-focused={isInputFocused}
-        onClick={focusInput}
-      >
-        <input
-          className={cn(
-            "bg-transparent flex-1 h-full px-3 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed"
-          )}
-          onFocus={handleInputFocus}
-          onBlur={handleInputBlur}
-          ref={inputRef}
-          {...props}
-        />
-        {trailing}
-      </div>
+        ref={ref}
+        {...props}
+      />
     );
   }
 );
