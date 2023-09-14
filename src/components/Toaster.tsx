e37bb3e3603b5ecccc7d51de6190454a67802585
Yunwoo Ji <yunwoo@ojware.com>
2023-09-14 16:47:30 +0900

    🐞 fix: move position of toast to center

--- src/components/Toaster.tsx
+++ src/components/Toaster.tsx
@@ -14,10 +14,14 @@ export function Toaster() {
   const { toasts } = useToast();
 
   return (
-    <ToastProvider>
+    <ToastProvider swipeDirection="up">
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
-          <Toast key={id} {...props}>
+          <Toast
+            key={id}
+            className="data-[swipe=move]:translate-y-[var(--radix-toast-swipe-move-y)] data-[swipe=cancel]:translate-y-0 data-[swipe=end]:translate-y-[var(--radix-toast-swipe-end-y)] data-[state=open]:slide-in-from-top-full data-[state=open]:sm:slide-in-from-top-full data-[state=closed]:sm:slide-out-to-right-0 data-[state=closed]:slide-out-to-top-full"
+            {...props}
+          >
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && (
@@ -29,7 +33,7 @@ export function Toaster() {
           </Toast>
         );
       })}
-      <ToastViewport />
+      <ToastViewport className="sm:bottom-auto sm:right-auto sm:top-16 sm:left-1/2 -translate-x-1/2 md:max-w-md" />
     </ToastProvider>
   );
 }
