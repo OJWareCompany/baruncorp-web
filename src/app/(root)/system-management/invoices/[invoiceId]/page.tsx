5dffda4980d4281cfdcdef9fd1c4bf5d561f67c5
Yunwoo Ji <yunwoo@ojware.com>
2023-10-05 16:30:08 +0900

    🐞 fix: fix property name

--- src/app/(root)/system-management/invoices/[invoiceId]/page.tsx
+++ src/app/(root)/system-management/invoices/[invoiceId]/page.tsx
@@ -424,7 +424,7 @@ export default function Page({ params: { invoiceId } }: Props) {
                     }
 
                     patchInvoiceIssueMutateAsync({
-                      files: [
+                      attachments: [
                         {
                           path: base64,
                         },
