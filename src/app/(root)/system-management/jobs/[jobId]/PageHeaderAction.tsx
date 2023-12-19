008eb10baba83af0b2099c68cf7ef5fb5d0eaff5
ojsangwon <sangwon@ojware.com>
2023-12-19 19:55:14 +0900

    🦄 refactor: fix deep link operation

--- src/app/(root)/system-management/jobs/[jobId]/PageHeaderAction.tsx
+++ src/app/(root)/system-management/jobs/[jobId]/PageHeaderAction.tsx
@@ -17,68 +17,51 @@ export default function PageHeaderAction({ job }: Props) {
         </Link>
       </Button>
       {/* TODO */}
-      <Button size={"sm"} variant={"outline"}>
+      <Button
+        size={"sm"}
+        variant={"outline"}
+        onClick={() => {
+          let openDesktopApp = false;
+
+          window.onblur = () => {
+            openDesktopApp = true;
+          };
+
+          setTimeout(() => {
+            if (!openDesktopApp) {
+              window.alert(
+                `Couldn't find the app.
+          Please go to the app installation page.
+          If it doesn't move, please check the pop-up.`
+              );
+              // TODO installUrl 교체 필요 and 환경 변수로 빼는 것도 고려
+              const installUrl = `http://ojw.synology.me:5000/sharing/amHYctCw5`;
+              const newWindow = window.open(installUrl, "_blank");
+              if (newWindow) {
+                newWindow.focus();
+              }
+            }
+          }, 2000);
+
+          const url = `barun://open-job?payload=${encodeURIComponent(
+            JSON.stringify({
+              organization: job.clientInfo.clientOrganizationName,
+              type: job.propertyType,
+              project: job.propertyFullAddress,
+              job: job.jobName,
+              // Hard data
+              // organization: "1 Earth Solar",
+              // type: "Commercial",
+              // project: "103 Church Street-Granville-Illinois-61326",
+              // job: "Job 1",
+            })
+          )}`;
+          window.location.href = url;
+        }}
+      >
         <FolderOpen className="mr-2 h-4 w-4" />
         <span>Open Folder</span>
       </Button>
     </div>
   );
 }
-
-/* --------------------------------- Legacy --------------------------------- */
-// <div className="flex gap-2">
-//   <Button asChild size={"sm"} variant={"outline"}>
-//     <Link href={`/system-management/jobs/${job.id}/ahj`}>
-//       <ScrollText className="mr-2 h-4 w-4" />
-//       <span>View AHJ Note</span>
-//     </Link>
-//   </Button>
-//   <Button
-//     size={"sm"}
-//     variant={"outline"}
-//     onClick={() => {
-//       let openDesktopApp = false;
-//
-//       window.onblur = () => {
-//         openDesktopApp = true;
-//       };
-//
-//       setTimeout(() => {
-//         if (!openDesktopApp) {
-//           window.alert(
-//             `Couldn't find the app.
-// Please go to the app installation page.
-// If it doesn't move, please check the pop-up.`
-//           );
-//           const installUrl = `http://ojw.synology.me:5000/sharing/amHYctCw5`;
-//           const newWindow = window.open(installUrl, "_blank");
-//           if (newWindow) {
-//             newWindow.focus();
-//           }
-//         }
-//       }, 2000);
-//
-//       const url = `barun://open-explorer?payload=${encodeURIComponent(
-//         JSON.stringify({
-//           organizationName: project?.clientOrganization,
-//           projectFolderName: project?.propertyAddress.fullAddress,
-//           jobFolderName: job?.jobName,
-//         })
-//       )}`;
-//       window.location.href = url;
-//     }}
-//   >
-//     <FolderOpen className="mr-2 h-4 w-4" />
-//     <span>Open Folder</span>
-//     {/* <a
-//     href={`barun://open-explorer?payload=${encodeURIComponent(
-//       JSON.stringify({
-//         organizationName: project?.clientOrganization,
-//         projectFolderName: project?.propertyAddress.fullAddress,
-//         jobFolderName: job?.jobName,
-//       })
-//     )}`}
-//   >
-//   </a> */}
-//   </Button>
-// </div>;
--- src/app/(root)/system-management/projects/[projectId]/PageHeaderAction.tsx
+++ src/app/(root)/system-management/projects/[projectId]/PageHeaderAction.tsx
@@ -17,58 +17,49 @@ export default function PageHeaderAction({ project }: Props) {
         </Link>
       </Button>
       {/* TODO */}
-      <Button size={"sm"} variant={"outline"}>
+      <Button
+        size={"sm"}
+        variant={"outline"}
+        onClick={() => {
+          let openDesktopApp = false;
+
+          window.onblur = () => {
+            openDesktopApp = true;
+          };
+
+          setTimeout(() => {
+            if (!openDesktopApp) {
+              window.alert(
+                `Couldn't find the app.
+        Please go to the app installation page.
+        If it doesn't move, please check the pop-up.`
+              );
+              // TODO installUrl 교체 필요 and 환경 변수로 빼는 것도 고려
+              const installUrl = `http://ojw.synology.me:5000/sharing/amHYctCw5`;
+              const newWindow = window.open(installUrl, "_blank");
+              if (newWindow) {
+                newWindow.focus();
+              }
+            }
+          }, 2000);
+
+          const url = `barun://open-project?payload=${encodeURIComponent(
+            JSON.stringify({
+              organization: project.clientOrganization,
+              type: project.propertyType,
+              project: project.propertyAddress.fullAddress,
+              // Hard data
+              // organization: "1 Earth Solar",
+              // type: "Commercial",
+              // project: "103 Church Street-Granville-Illinois-61326",
+            })
+          )}`;
+          window.location.href = url;
+        }}
+      >
         <FolderOpen className="mr-2 h-4 w-4" />
         <span>Open Folder</span>
       </Button>
     </div>
   );
 }
-
-/* --------------------------------- Legacy --------------------------------- */
-// <Button
-// size={"sm"}
-// variant={"outline"}
-// onClick={() => {
-//   let openDesktopApp = false;
-//
-//   window.onblur = () => {
-//     openDesktopApp = true;
-//   };
-//
-//   setTimeout(() => {
-//     if (!openDesktopApp) {
-//       window.alert(
-//         `Couldn't find the app.
-// Please go to the app installation page.
-// If it doesn't move, please check the pop-up.`
-//       );
-//       const installUrl = `http://ojw.synology.me:5000/sharing/amHYctCw5`;
-//       const newWindow = window.open(installUrl, "_blank");
-//       if (newWindow) {
-//         newWindow.focus();
-//       }
-//     }
-//   }, 2000);
-//
-//   const url = `barun://open-explorer?payload=${encodeURIComponent(
-//     JSON.stringify({
-//       organizationName: project.clientOrganization,
-//       projectFolderName: project.propertyAddress.fullAddress,
-//     })
-//   )}`;
-//   window.location.href = url;
-// }}
-// >
-// <FolderOpen className="mr-2 h-4 w-4" />
-// <span>Open Folder</span>
-// {/* <a
-//         href={`barun://open-explorer?payload=${encodeURIComponent(
-//           JSON.stringify({
-//             organizationName: project.clientOrganization,
-//             projectFolderName: project.propertyAddress.fullAddress,
-//           })
-//         )}`}
-//       >
-//       </a> */}
-// </Button>
