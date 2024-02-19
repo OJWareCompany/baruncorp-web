5bfbfce40dc28bba5b6fc5aaee7160ac4a48667e
ojsangwon <sangwon@ojware.com>
2024-02-19 17:11:45 +0900

    🐳 chore: add geoId field to openAHjFolder

--- src/components/OpenAhjFolderButton.tsx
+++ src/components/OpenAhjFolderButton.tsx
@@ -6,17 +6,25 @@ import { openAhjFolder } from "@/lib/deeplink";
 
 interface Props {
   ahjNote: AhjNoteResponseDto;
+  geoId: string;
   className?: string;
 }
 
-export default function OpenAhjFolderButton({ className, ahjNote }: Props) {
+export default function OpenAhjFolderButton({
+  className,
+  ahjNote,
+  geoId,
+}: Props) {
   return (
     <Button
       size={"sm"}
       variant={"outline"}
       className={className}
       onClick={() => {
-        openAhjFolder({ fullAhjName: ahjNote.general.fullAhjName });
+        openAhjFolder({
+          geoId,
+          fullAhjName: ahjNote.general.fullAhjName,
+        });
       }}
     >
       <FolderOpen className="mr-2 h-4 w-4" />
