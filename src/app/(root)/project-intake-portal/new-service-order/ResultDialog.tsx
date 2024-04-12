226b7a3255685783733e85129f1d907a91f17a5e
ojyochan <162090034+ojyochan@users.noreply.github.com>
2024-04-12 17:29:18 +0900

    ✨ feat: file system Add exception handling for missing files

--- src/app/(root)/project-intake-portal/new-service-order/ResultDialog.tsx
+++ src/app/(root)/project-intake-portal/new-service-order/ResultDialog.tsx
@@ -143,22 +143,22 @@ export default function ResultDialog({ state, ...dialogProps }: Props) {
                 ? postJobFilesProgress.value === 100
                   ? "You can place more orders or go to the details page for your order details." // 업로드가 끝나면
                   : "It may take some time for files to appear in Google Drive after uploading." // 업로드 중 일때
-                : ""
-              : myOrder === 0
-              ? "It may take some time for files to appear in Google Drive after uploading."
-              : "Order completed, but please wait a moment for file upload."
+                : myOrder === 0
+                ? "It may take some time for files to appear in Google Drive after uploading."
+                : "Order completed, but please wait a moment for file upload."
+              : state.open && state.files.length === 0
               ? "You can place more orders or go to the details page for your order details."
-              : ""}
+              : "Order completed, but please wait a moment for file upload."}
             <br />
             <span className="text-muted-foreground text-bold text-gray-700 font-medium">
               {activeFileUpload
                 ? ""
-                : myOrder === 0 && state.open && state.files.length !== 0
+                : state.open && state.files.length === 0
+                ? ""
+                : myOrder === 0
                 ? ""
                 : "Your turn for file upload: " +
-                  (myOrder ?? "Getting my-order...")
-                ? ""
-                : ""}
+                  (myOrder ?? "Getting my-order...")}
             </span>
           </DialogDescription>
         </DialogHeader>
