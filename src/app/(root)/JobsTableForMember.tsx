fc221700673049154a52bf742b7b07fbd15158b5
ojyochan <162090034+ojyochan@users.noreply.github.com>
2024-04-15 16:35:22 +0900

    ✨ feat: add a global search (select optionX)

--- src/app/(root)/JobsTableForMember.tsx
+++ src/app/(root)/JobsTableForMember.tsx
@@ -79,7 +79,6 @@ import usePatchJobSendMutation from "@/mutations/usePatchJobSendMutation";
 import { toast } from "@/components/ui/use-toast";
 import NewTabTableRow from "@/components/table/NewTabTableRow";
 import { InTableButton } from "@/components/ui/intablebutton";
-import NameSearch from "@/components/table/NameSearch";
 import OpenJobFolderOnWebButton from "@/components/job-detail-page/OpenJobFolderOnWebButton";
 
 const columnHelper =
@@ -122,6 +121,10 @@ export default function JobsTableForMember({ type }: Props) {
   const prioritySearchParamName = `${TABLE_NAME}${type}Priority`;
   const projectNumberSearchParamName = `${TABLE_NAME}${type}ProjectNumber`;
   const propertyOwnerSearchParamName = `${TABLE_NAME}${type}PropertyOwner`;
+  const globalJobNameSearchParamName = `${TABLE_NAME}JobName`;
+  const globalProjectNumberSearchParamName = `${TABLE_NAME}ProjectNumber`;
+  const globalPropertyOwnerSearchParamName = `${TABLE_NAME}PropertyOwner`;
+  // const globalPageIndexSearchParamName = `${TABLE_NAME}PageIndex`;
 
   const [pageSize, setPageSize] = useLocalStorage<number>(
     `${RELATIVE_PATH}_${type}`,
@@ -177,6 +180,24 @@ export default function JobsTableForMember({ type }: Props) {
     searchParams.get(encodeURIComponent(projectNumberSearchParamName)) ?? "";
   const propertyOwnerSearchParam =
     searchParams.get(encodeURIComponent(propertyOwnerSearchParamName)) ?? "";
+  const globalJobNameSearchParam =
+    searchParams.get(encodeURIComponent(globalJobNameSearchParamName)) ?? "";
+  const globalProjectNumberSearchParam =
+    searchParams.get(encodeURIComponent(globalProjectNumberSearchParamName)) ??
+    "";
+  const globalPropertyOwnerSearchParam =
+    searchParams.get(encodeURIComponent(globalPropertyOwnerSearchParamName)) ??
+    "";
+  // const globalPagination: PaginationState = {
+  //   pageIndex: searchParams.get(
+  //     encodeURIComponent(globalPageIndexSearchParamName)
+  //   )
+  //     ? Number(
+  //         searchParams.get(encodeURIComponent(globalPageIndexSearchParamName))
+  //       )
+  //     : 0,
+  //   pageSize,
+  // };
 
   const onPaginationChange = useOnPaginationChange({
     pageIndexSearchParamName,
@@ -189,7 +210,7 @@ export default function JobsTableForMember({ type }: Props) {
     () => ({
       page: pagination.pageIndex + 1,
       limit: pagination.pageSize,
-      jobName: jobNameSearchParam,
+      jobName: jobNameSearchParam || globalJobNameSearchParam,
       jobStatus:
         transformJobStatusEnumWithEmptyStringIntoNullableJobStatusEnum.parse(
           jobStatusSearchParam
@@ -214,13 +235,16 @@ export default function JobsTableForMember({ type }: Props) {
         transformJobPriorityEnumWithEmptyStringIntoNullableJobPriorityEnum.parse(
           prioritySearchParam
         ),
-      projectNumber: projectNumberSearchParam,
-      propertyOwner: propertyOwnerSearchParam,
+      projectNumber: projectNumberSearchParam || globalProjectNumberSearchParam,
+      propertyOwner: propertyOwnerSearchParam || globalPropertyOwnerSearchParam,
     }),
     [
       pagination.pageIndex,
       pagination.pageSize,
+      // globalPagination.pageIndex,
+      // globalPagination.pageSize,
       jobNameSearchParam,
+      globalJobNameSearchParam,
       jobStatusSearchParam,
       mountingTypeSearchParam,
       propertyTypeSearchParam,
@@ -228,7 +252,9 @@ export default function JobsTableForMember({ type }: Props) {
       inReviewSearchParam,
       prioritySearchParam,
       projectNumberSearchParam,
+      globalProjectNumberSearchParam,
       propertyOwnerSearchParam,
+      globalPropertyOwnerSearchParam,
     ]
   );
 
@@ -577,14 +603,14 @@ export default function JobsTableForMember({ type }: Props) {
   return (
     <div className="space-y-2">
       <div>
-        <NameSearch
+        {/* <NameSearch
           searchParamOptions={{
-            jobNameSearchParamName: jobNameSearchParamName,
-            projectNumberSearchParamName: projectNumberSearchParamName,
-            propertyOwnerSearchParamName: propertyOwnerSearchParamName,
+            jobNameSearchParamName: globalJobNameSearchParamName,
+            projectNumberSearchParamName: globalProjectNumberSearchParamName,
+            propertyOwnerSearchParamName: globalPropertyOwnerSearchParamName,
           }}
           pageIndexSearchParamName={pageIndexSearchParamName}
-        />
+        /> */}
       </div>
       <div className="rounded-md border overflow-hidden">
         <Table>
