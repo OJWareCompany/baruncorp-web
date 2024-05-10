6688586fbb004db17b73f1d0a41b3852f716808c
ojyochan <162090034+ojyochan@users.noreply.github.com>
2024-05-10 19:02:33 +0900

    ✨ feat: add issued client invoice history

--- src/api/api-spec.ts
+++ src/api/api-spec.ts
@@ -1104,7 +1104,8 @@ export interface ClientToInvoiceResponseDto {
 
 export interface IssueInvoiceRequestDto {
   files: File[];
-  cc: string[][];
+  /** @default ["hyomin@oj.vision"] */
+  cc?: string[];
 }
 
 export interface ClientWithOutstandingBalancesResponseDto {
@@ -3070,15 +3071,9 @@ export interface FindJobPaginatedHttpControllerFindJobParams {
    * @default "3480 Northwest 33rd Court"
    */
   jobName?: string | null;
-  /**
-   * Using LIKE (중간 값 검색)
-   * @default ""
-   */
+  /** Using LIKE (중간 값 검색) */
   projectNumber?: string | null;
-  /**
-   * Using LIKE (중간 값 검색)
-   * @default ""
-   */
+  /** Using LIKE (중간 값 검색) */
   propertyFullAddress?: string | null;
   /** @default "Commercial" */
   projectPropertyType?: "Residential" | "Commercial" | null;
@@ -3100,11 +3095,16 @@ export interface FindJobPaginatedHttpControllerFindJobParams {
   inReview?: boolean | null;
   /** @default "Medium" */
   priority?: "Immediate" | "High" | "Medium" | "Low" | "None" | null;
-  /**
-   * Using LIKE (중간 값 검색)
-   * @default ""
-   */
+  /** Using LIKE (중간 값 검색) */
   propertyOwner?: string | null;
+  /** Using LIKE (중간 값 검색) */
+  taskName?: string | null;
+  /** Using LIKE (중간 값 검색) */
+  taskAssigneeName?: string | null;
+  /** Using LIKE (중간 값 검색) */
+  clientOrganizationName?: string | null;
+  dateSentToClientStart?: string | null;
+  dateSentToClientEnd?: string | null;
   sortField?:
     | "dateSentToClient"
     | "completedCancelledDate"
