d0dc2064923a56829bc7e716330b979038fed024
ojyochan <yohan1101@oj.vision>
2024-05-15 01:21:21 +0900

    ✨ feat: something

--- src/components/client-invoice-detail-page/IssueButton.tsx
+++ src/components/client-invoice-detail-page/IssueButton.tsx
@@ -19,14 +19,6 @@ import {
 import { Input } from "../ui/input";
 import ClientInvoiceDocument from "./ClientInvoiceDocument";
 import {
-  AlertDialog,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-} from "@/components/ui/alert-dialog";
-import {
   Dialog,
   DialogContent,
   DialogFooter,
@@ -72,9 +64,6 @@ export default function IssueButton({
   const [open, setOpen] = useState(false);
 
   const getFieldValues = (data: InvoiceResponseDto): FieldValues => {
-    console.log("### getFieldValues ###");
-    console.log(data.issueHistory);
-    console.log(data.currentCc);
     if (!data.issueHistory) {
       return { ccArray: [] };
     }
@@ -134,10 +123,6 @@ export default function IssueButton({
     const file = new File([blob], fileName, {
       type: blob.type,
     });
-    console.log(
-      "cc",
-      values.ccArray.map((cc) => cc.cc)
-    );
     patchClientInvoiceIssueMutateAsync({
       files: [file],
       cc: values.ccArray.map((cc) => cc.cc),
@@ -168,50 +153,42 @@ export default function IssueButton({
 
   return (
     <>
-      <Button
-        onClick={() => {
-          setOpen(true);
-        }}
-        variant={"outline"}
-        size={"sm"}
-        className="h-[28px] text-xs px-2"
-        disabled={instance.loading}
-      >
-        {clientInvoice.status === "Unissued" ? (
-          <ArrowUp className="mr-2 h-4 w-4" />
-        ) : (
-          <Mail className="mr-2 h-4 w-4" />
-        )}
-        {clientInvoice.status === "Unissued" ? "Issue" : "Remind"}
-      </Button>
-      <AlertDialog open={open} onOpenChange={setOpen}>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you sure?</AlertDialogTitle>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <Dialog>
-              <DialogTrigger asChild>
-                <Button>Continue</Button>
-              </DialogTrigger>
-              <DialogContent className="sm:max-w-[500px]">
-                <DialogHeader>
-                  <DialogTitle>Are You Sure?</DialogTitle>
-                </DialogHeader>
-                <Form {...form}>
-                  <form
-                    onSubmit={form.handleSubmit(onSubmit)}
-                    className="grid gap-4 py-4"
-                  >
-                    <FormField
-                      control={form.control}
-                      name="ccArray"
-                      render={() => {
-                        return (
-                          <FormItem>
-                            <FormLabel>Cc</FormLabel>
-                            {/* {fields.map((field, index) => (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button
+            onClick={() => {
+              setOpen(true);
+            }}
+            variant={"outline"}
+            size={"sm"}
+            className="h-[28px] text-xs px-2"
+            disabled={instance.loading}
+          >
+            {clientInvoice.status === "Unissued" ? (
+              <ArrowUp className="mr-2 h-4 w-4" />
+            ) : (
+              <Mail className="mr-2 h-4 w-4" />
+            )}
+            {clientInvoice.status === "Unissued" ? "Issue" : "Remind"}
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[500px]">
+          <DialogHeader>
+            <DialogTitle>Are You Sure?</DialogTitle>
+          </DialogHeader>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="grid gap-4 py-4"
+            >
+              <FormField
+                control={form.control}
+                name="ccArray"
+                render={() => {
+                  return (
+                    <FormItem>
+                      <FormLabel>Cc</FormLabel>
+                      {/* {fields.map((field, index) => (
                               <FormField
                                 key={field.id}
                                 control={form.control}
@@ -240,134 +217,122 @@ export default function IssueButton({
                                 }}
                               />
                             ))} */}
-                            {fields.map((field, index) => (
-                              <FormField
-                                key={field.id}
-                                control={form.control}
-                                name={`ccArray.${index}.cc`}
-                                render={({ field }) => {
-                                  return (
-                                    <FormItem>
-                                      <div className="flex flex-row gap-2">
-                                        <FormControl>
-                                          <Input {...field} />
-                                        </FormControl>
-                                        {index}
-                                        <Button
-                                          variant={"outline"}
-                                          size={"icon"}
-                                          className="flex-shrink-0"
-                                          onClick={() => {
-                                            if (fields.length === 1) {
-                                              form.setValue(
-                                                `ccArray.${index}.cc`,
-                                                ""
-                                              ); // 값을 초기화
-                                              return;
-                                            }
-                                            remove(index);
-                                          }}
-                                        >
-                                          <X className="h-4 w-4" />
-                                        </Button>
-                                      </div>
-                                      <FormMessage />
-                                    </FormItem>
-                                  );
-                                }}
-                              />
-                            ))}
-                            <Button
-                              variant={"outline"}
-                              className="w-full"
-                              onClick={() => {
-                                console.log("추가 append");
-                                append({ cc: "" });
-                              }}
-                              type="button"
-                            >
-                              Add Email
-                            </Button>
-                          </FormItem>
-                        );
-                      }}
-                    />
-                    <DialogFooter>
+                      {fields.map((field, index) => (
+                        <FormField
+                          key={field.id}
+                          control={form.control}
+                          name={`ccArray.${index}.cc`}
+                          render={({ field }) => {
+                            return (
+                              <FormItem>
+                                <div className="flex flex-row gap-2">
+                                  <FormControl>
+                                    <Input {...field} />
+                                  </FormControl>
+                                  <Button
+                                    variant={"outline"}
+                                    size={"icon"}
+                                    className="flex-shrink-0"
+                                    onClick={() => {
+                                      if (fields.length === 1) {
+                                        form.setValue(
+                                          `ccArray.${index}.cc`,
+                                          ""
+                                        ); // 값을 초기화
+                                        return;
+                                      }
+                                      remove(index);
+                                    }}
+                                  >
+                                    <X className="h-4 w-4" />
+                                  </Button>
+                                </div>
+                                <FormMessage />
+                              </FormItem>
+                            );
+                          }}
+                        />
+                      ))}
                       <Button
                         variant={"outline"}
+                        className="w-full"
                         onClick={() => {
-                          const { blob } = instance;
-                          if (blob == null) {
-                            return;
-                          }
-
-                          const fileName = `[Barun Corp] ${
-                            clientInvoice.clientOrganization.name
-                          }, ${format(
-                            new Date(
-                              clientInvoice.servicePeriodDate.slice(0, 7)
-                            ),
-                            "MMM yyyy"
-                          )}, Client Invoice.pdf`;
-
-                          // Blob을 File로 변환
-                          const file = new File([blob], fileName, {
-                            type: blob.type,
-                          });
-
-                          patchClientInvoiceIssueMutateAsync({
-                            files: [file],
-                          })
-                            .then(() => {
-                              queryClient.invalidateQueries({
-                                queryKey: getClientInvoiceQueryKey(
-                                  clientInvoice.id
-                                ),
-                              });
-                              toast({
-                                title: "Success",
-                              });
-                              setOpen(false);
-                              remove(
-                                Array.from(
-                                  { length: fields.length },
-                                  (_, i) => i
-                                )
-                              );
-                              append({ cc: "" });
-                            })
-                            .catch((error: AxiosError<ErrorResponseData>) => {
-                              if (
-                                error.response &&
-                                error.response.data.errorCode.filter(
-                                  (value) => value != null
-                                ).length !== 0
-                              ) {
-                                toast({
-                                  title: error.response.data.message,
-                                  variant: "destructive",
-                                });
-                                return;
-                              }
-                            });
+                          append({ cc: "" });
                         }}
+                        type="button"
                       >
-                        No Cc
+                        Add Email
                       </Button>
-                      <LoadingButton
-                        type="submit"
-                        isLoading={form.formState.isSubmitting}
-                      >
-                        Submit
-                      </LoadingButton>
-                    </DialogFooter>
-                  </form>
-                </Form>
-              </DialogContent>
-            </Dialog>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+                    </FormItem>
+                  );
+                }}
+              />
+              <DialogFooter>
+                <Button
+                  variant={"outline"}
+                  onClick={() => {
+                    const { blob } = instance;
+                    if (blob == null) {
+                      return;
+                    }
+
+                    const fileName = `[Barun Corp] ${
+                      clientInvoice.clientOrganization.name
+                    }, ${format(
+                      new Date(clientInvoice.servicePeriodDate.slice(0, 7)),
+                      "MMM yyyy"
+                    )}, Client Invoice.pdf`;
+
+                    // Blob을 File로 변환
+                    const file = new File([blob], fileName, {
+                      type: blob.type,
+                    });
+
+                    patchClientInvoiceIssueMutateAsync({
+                      files: [file],
+                    })
+                      .then(() => {
+                        queryClient.invalidateQueries({
+                          queryKey: getClientInvoiceQueryKey(clientInvoice.id),
+                        });
+                        toast({
+                          title: "Success",
+                        });
+                        setOpen(false);
+                        remove(
+                          Array.from({ length: fields.length }, (_, i) => i)
+                        );
+                        append({ cc: "" });
+                      })
+                      .catch((error: AxiosError<ErrorResponseData>) => {
+                        if (
+                          error.response &&
+                          error.response.data.errorCode.filter(
+                            (value) => value != null
+                          ).length !== 0
+                        ) {
+                          toast({
+                            title: error.response.data.message,
+                            variant: "destructive",
+                          });
+                          return;
+                        }
+                      });
+                  }}
+                >
+                  Submit (No Cc)
+                </Button>
+                <LoadingButton
+                  type="submit"
+                  isLoading={form.formState.isSubmitting}
+                >
+                  Submit
+                </LoadingButton>
+              </DialogFooter>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
